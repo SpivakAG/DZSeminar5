5013bbffe36539cd1d278cd11c3981d5f3bbe8a6
ArtemSpivak <spivak25@yandex.ru>
2022-04-25 23:26:25 +0300

    задача решена

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,1 +1,30 @@
 ﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = new int[4];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-99, 100);
+}
+
+int oddPositionSum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        oddPositionSum += array[i];
+    }
+}
+PrintArray(array);
+
+Console.WriteLine($" Количество четных чисел в массиве равно {oddPositionSum}");
+
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(col[i]);
+        if (i < array.Length - 1) Console.Write(", ");
+    }
+}
